13ba6e8cdda107d2ea1bf4b5078aef719b643475
skassanjee <skassanjee@gmail.com>
2022-01-17 16:00:11 -0700

    hash tables

---
+++ leetcode/2sum.js
@@ -0,0 +1,17 @@
+const target = 9
+const arr1 = [1, 2, 3, 4, 5]
+
+const twoSum = (array ) => {
+const obj = {}
+
+for(i = 0; i< array.length; i++){
+    const needed = target - array[i]
+    if(!obj[needed]){
+        obj[array[i]] = i
+    }else if (obj[needed]){
+        return [obj[needed], i ]
+    }
+}
+}
+
+console.log(twoSum(arr1))
